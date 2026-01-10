a429730e7de4c095738fa1c5aa713ff7acbb06be
Kevinsalinas-07 <00086125@uca.edu.sv>
2026-01-09 22:57:43 -0600

    add: Se agrego funcion division

--- Calculador.cpp
+++ Calculador.cpp
@@ -5,6 +5,7 @@ using namespace std;
 float sumar(float a, float b);
 float restar(float a, float b);
 float multiplicar(float a, float b);
+float dividir(float a, float b);
 
 int main()
 {
@@ -20,7 +21,16 @@ float restar(float a, float b)
 {
     return a - b;
 }
-float multiplicar(float a, float b) 
-{ 
+float multiplicar(float a, float b)
+{
     return a * b;
+}
+float dividir(float a, float b)
+{
+    if (b == 0)
+    {
+        cout << "Error. No se puede dividir entre 0" << endl;
+        return 0;
+    }
+    return a / b;
 }
